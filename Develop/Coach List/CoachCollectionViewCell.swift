50c3c6e421e6055a459989d238f51850a0693fa4
Christopher Truman <christopher.truman@sony.com>
2019-08-12 21:37:38 -0700

    App Icon

---
+++ Develop/Coach List/CoachCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CoachCollectionViewCell.swift
+//  Develop
+//
+//  Created by Truman, Christopher on 8/12/19.
+//  Copyright © 2019 Truman. All rights reserved.
+//
+
+import UIKit
+
+class CoachCollectionViewCell: UICollectionViewCell {
+    
+}
